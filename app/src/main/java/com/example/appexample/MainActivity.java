7880ec4b3436b7aa3bd36cc94d6f9bdc68bf2278
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2022-02-02 23:52:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appexample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.appexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnAl, btnDa;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAl=(Button) findViewById(R.id.Alim);
+        btnDa=(Button) findViewById(R.id.Agua);
+        btnAl.setOnClickListener(this);
+        btnDa.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==btnAl.getId()){
+            Toast.makeText(getApplicationContext(), "Alimentando a Pou",Toast.LENGTH_LONG).show();
+        }
+        if (view.getId()==R.id.Agua){
+            Toast.makeText(getApplicationContext(), "Hidratando a Pou",Toast.LENGTH_LONG).show();
+        }
+    }
+}
