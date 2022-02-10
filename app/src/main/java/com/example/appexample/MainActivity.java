636b3e6cb5f6fee8035386e12ba4ea3686932ca6
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2022-02-10 18:32:06 -0500

    third commit

--- app/src/main/java/com/example/appexample/MainActivity.java
+++ app/src/main/java/com/example/appexample/MainActivity.java
@@ -5,28 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnAl, btnDa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAl=(Button) findViewById(R.id.Alim);
-        btnDa=(Button) findViewById(R.id.Agua);
+        btnAl= findViewById(R.id.Alim);
+        btnDa= findViewById(R.id.Agua);
         btnAl.setOnClickListener(this);
         btnDa.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
+        ImageView img1 = findViewById(R.id.comida);
+        ImageView img2 = findViewById(R.id.bagua);
         if (view.getId()==btnAl.getId()){
             Toast.makeText(getApplicationContext(), "Alimentando a Pou",Toast.LENGTH_LONG).show();
+            img1.setVisibility(View.INVISIBLE);
+
         }
         if (view.getId()==R.id.Agua){
             Toast.makeText(getApplicationContext(), "Hidratando a Pou",Toast.LENGTH_LONG).show();
+            img2.setVisibility(View.INVISIBLE);
+
         }
+
     }
 }
